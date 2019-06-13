a82fc54759681c7839d7fe9e931373adc25dd718
Jack Kinsey <jack@hexbitstudios.com>
2019-06-12 21:08:24 -0500

    Adds a more interesting complex starter scene

--- src/index.js
+++ src/index.js
@@ -3,8 +3,17 @@ import 'phaser';
 import SimpleScene from './scenes/simple-scene';
 
 const gameConfig = {
-  width: 680,
-  height: 400,
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: {
+        y: 200,
+      },
+    },
+  },
   scene: SimpleScene,
 };
 
